80aa62c0499e090ba7a1765b6b56f8bb1f14fe14
Aliaksei Vasilkou <Aliaksei.Vasilkou@ihg.com>
2023-09-06 21:35:32 +0200

    initial commit

---
+++ src/main/java/com/epam/jmp/nosql/elasticsearch/JmpNosqlElasticsearchApplication.java
@@ -0,0 +1,13 @@
+package com.epam.jmp.nosql.elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmpNosqlElasticsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmpNosqlElasticsearchApplication.class, args);
+    }
+
+}
